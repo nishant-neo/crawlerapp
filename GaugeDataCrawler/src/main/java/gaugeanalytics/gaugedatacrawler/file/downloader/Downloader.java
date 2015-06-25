0be2a6b73d4e00fff1f45b14ea8f41038c5f03fb
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-06-25 14:38:27 +0530

    Updating code

---
+++ GaugeDataCrawler/src/main/java/gaugeanalytics/gaugedatacrawler/file/downloader/Downloader.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gaugeanalytics.gaugedatacrawler.file.downloader;
+
+/**
+ *
+ * @author Abhay
+ */
+public interface Downloader {
+
+    public void download();
+}
