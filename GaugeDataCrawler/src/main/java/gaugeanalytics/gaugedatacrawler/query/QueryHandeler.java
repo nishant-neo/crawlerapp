b8aaa6f2da1fdf88271c6b4cd02c984aa3a90944
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-06-25 17:20:36 +0530

    Updating Code

--- GaugeDataCrawler/src/main/java/gaugeanalytics/gaugedatacrawler/query/QueryHandeler.java
+++ GaugeDataCrawler/src/main/java/gaugeanalytics/gaugedatacrawler/query/QueryHandeler.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package gaugeanalytics.gaugedatacrawler.scrapper;
+package gaugeanalytics.gaugedatacrawler.query;
 
 /**
  *
  * @author Abhay
  */
-public class ScrapperException extends Exception {
+public class QueryHandeler {
     
 }
