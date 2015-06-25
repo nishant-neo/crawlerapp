c8ec12cb338d1a7c52b0b6053c57d32ca073d5c6
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-06-25 12:19:51 +0530

    test

---
+++ GaugeDataCrawler/src/main/java/gaugeanalytics/gaugedatacrawler/Thread/DataThread.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gaugeanalytics.gaugedatacrawler.Thread;
+
+/**
+ *
+ * @author Abhay
+ */
+public class DataThread implements Runnable{
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ GaugeDataCrawler/src/main/java/gaugeanalytics/gaugedatacrawler/validator/UrlValidator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gaugeanalytics.gaugedatacrawler.validator;
+
+/**
+ *
+ * @author Abhay
+ */
+public class UrlValidator implements Validator {
+
+    @Override
+    public boolean isValid(Object field) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
