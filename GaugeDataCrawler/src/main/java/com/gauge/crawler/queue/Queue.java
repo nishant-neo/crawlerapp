a18d7d36ce9d5b0c070d7ce0c93d419952e732b5
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-06-27 09:28:57 +0530

    Updating Code

--- GaugeDataCrawler/src/main/java/com/gauge/crawler/queue/Queue.java
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/queue/Queue.java
@@ -1,5 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.gauge.crawler.queue;
 
+/**
+ *
+ * @author Abhay
+ */
 public class Queue {
 
+   
 }
