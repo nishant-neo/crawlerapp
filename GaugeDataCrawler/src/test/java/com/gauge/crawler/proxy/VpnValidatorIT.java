311f21e21ea6205a1f62e3bc007286724c47cb4c
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-07-07 13:35:03 +0530

    Updating Code

--- GaugeDataCrawler/src/test/java/com/gauge/crawler/proxy/VpnValidatorIT.java
+++ GaugeDataCrawler/src/test/java/com/gauge/crawler/proxy/VpnValidatorIT.java
@@ -5,6 +5,8 @@
  */
 package com.gauge.crawler.proxy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,14 +17,18 @@ import static org.junit.Assert.*;
  * @author Abhay
  */
 public class VpnValidatorIT {
-    
+
+    //public static ArrayList<String> vpnList;
     public VpnValidatorIT() {
+        // vpnList = new ArrayList();
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
+        // String[] list = {"183.207.229.204:9000", "120.203.148.7:8118", "103.249.91.193:8080", "61.234.249.125:8118", "42.120.81.28:80"};
+        // vpnList.addAll(Arrays.asList(list));
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
@@ -32,14 +38,16 @@ public class VpnValidatorIT {
      */
     @Test
     public void testIsValid() {
+
         System.out.println("isValid");
-        Object vpn = null;
+        String vpn = "183.207.229.204:9000";
         VpnValidator instance = new VpnValidator();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.isValid(vpn);
         assertEquals(expResult, result);
+
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
-    
+
 }
