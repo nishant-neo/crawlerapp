35faf12e266302fa30d8635fed1991f76abbb12e
Nishant Oli <nishantoli95@gmail.com>
2015-07-08 10:02:41 +0530

    changes to robottxt downloader

--- GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/downloader/RobotTxtDownloader.java
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/downloader/RobotTxtDownloader.java
@@ -4,18 +4,42 @@
  * and open the template in the editor.
  */
 package com.gauge.crawler.webpage.downloader;
-
+import java.net.URL;
+import java.util.*;
+import java.io.*;
 /**
  *
  * @author Abhay
  */
-
-// This calss will responsible for downloading and saving the Robot.Txt file
+// This class will responsible for downloading and saving the Robot.Txt file
 public class RobotTxtDownloader implements Downloader {
 
     @Override
-    public void download(Object url) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void download(Object url)throws Exception {
+        URL link = new URL((String) url + "/robots.txt");
+        InputStream in = link.openStream();
+        String inputStreamString = new Scanner(in,"UTF-8").useDelimiter("\\A").next();
+        System.out.println(inputStreamString);
+        FileOutputStream fop = null;
+		File file;
+		String content = inputStreamString;
+                try {
+                        file = new File("C:\\Users\\NISHANT\\Documents\\NetBeansProjects\\gauge-data\\GaugeDataCrawler\\Program-File\\link"+"vfv"+".txt");
+                        fop = new FileOutputStream(file);
+                        // if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+                        // get the content in bytes
+			byte[] contentInBytes = content.getBytes();
+                        fop.write(contentInBytes);
+			fop.flush();
+			fop.close();
+                        //System.out.println("Done");
+                } 
+                catch (IOException e) 
+                {
+			e.printStackTrace();
+                }
     }
-    
 }
