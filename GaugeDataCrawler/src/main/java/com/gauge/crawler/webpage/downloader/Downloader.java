9e08c31f13c07dc4c5fcbd60d8e5936db2871a74
Nishant Oli <nishantoli95@gmail.com>
2015-07-08 10:01:56 +0530

    addition to vpn and some changes in robot.txt validator

--- GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/downloader/Downloader.java
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/downloader/Downloader.java
@@ -14,5 +14,5 @@ package com.gauge.crawler.webpage.downloader;
 // This interface has one method called download and it will extended by Downlder class
 public interface Downloader {
 
-    public void download(Object url);
+    public void download(Object url)throws Exception;
 }
