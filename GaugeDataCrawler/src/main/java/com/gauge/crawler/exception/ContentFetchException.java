608b7ad3f79e3716419cef726ccb65c8591e330d
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-07-10 10:52:15 +0530

    Updating Code

---
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/exception/ContentFetchException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gauge.crawler.exception;
+
+/**
+ *
+ * @author Abhay
+ * Thrown when there is a problem with the content fetching 
+ */
+public class ContentFetchException extends Exception {
+    
+}
---
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/exception/GaugeCrawlerException.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gauge.crawler.exception;
+
+/**
+ *
+ * @author Abhay
+ */
+// This Exception class will handle exception in crawler
+public class GaugeCrawlerException extends Exception {
+
+}
