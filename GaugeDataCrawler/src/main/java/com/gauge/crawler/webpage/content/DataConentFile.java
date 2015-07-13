30757a6600e81c86fd9f86c79da2c3cb089e0995
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-07-13 16:37:19 +0530

    Updating Code

--- GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/content/DataConentFile.java
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/content/DataConentFile.java
@@ -5,7 +5,22 @@
  */
 package com.gauge.crawler.webpage.content;
 
+import com.gauge.crawler.browser.BrowserAgent;
 import com.gauge.crawler.commons.Content;
+import com.gauge.crawler.gaugefile.FilePathHandeler;
+import com.gauge.crawler.url.urlqueue.UrlQueue;
+import com.jaunt.Elements;
+import com.jaunt.ResponseException;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.pool.impl.SoftReferenceObjectPool;
 
 /**
  *
@@ -14,19 +29,61 @@ import com.gauge.crawler.commons.Content;
 // This class will responsible for extracting the Meta Data from webpage , and this class is implementing Content interface and overriding the extract method
 public class DataConentFile implements Content {
 
+    private static UrlQueue urlQueue;
+    BrowserAgent browserAgent;
+    SoftReferenceObjectPool pool;
+    String textData;          // Text data extracted,
+    ArrayList<String> xPathList;// List of Xpath
+    Elements elements;
+    int xPathMethodCounter;
+    FilePathHandeler filePathHandeler;
+    Writer writer;
+
+    public DataConentFile(SoftReferenceObjectPool pool) throws Exception {
+        this.pool = pool;
+        browserAgent = (BrowserAgent) pool.borrowObject();
+        urlQueue = new UrlQueue();
+        this.textData = null;
+        xPathList = new ArrayList();
+        elements = null;
+        xPathMethodCounter = 0;
+        filePathHandeler = new FilePathHandeler();
+    }
+
+    public void openWebSite(String url) throws ResponseException {
+        browserAgent.visit(url);
+    }
+
     @Override
     public void extractData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-
+        this.elements = this.browserAgent.doc.findEvery(this.getXpath());
+        this.textData = this.elements.innerText();
     }
 
     @Override
     public void saveData() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(this.filePathHandeler.getTextFilePath("url")), "utf-8"));
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(DataConentFile.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(DataConentFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     // This method will convert text fro pdf
     public void getTextFromPdf() {
+
+    }
+
+    public void closeBrowser() throws Exception {
+        pool.returnObject(this.browserAgent);
+    }
+
+    private String getXpath() {
+        String xPath = this.xPathList.get(xPathMethodCounter);
+        this.xPathMethodCounter++;
+        return xPath;
 
     }
 
