3b9f63df1779284e48ad9b8dcdca89179040720a
Abhay Kumar <abhay.kumar@gaugeanalytics.com>
2015-07-15 11:09:27 +0530

    Updating Code

--- GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/content/DataConentFile.java
+++ GaugeDataCrawler/src/main/java/com/gauge/crawler/webpage/content/DataConentFile.java
@@ -8,16 +8,21 @@ package com.gauge.crawler.webpage.content;
 import com.gauge.crawler.browser.BrowserAgent;
 import com.gauge.crawler.browser.BrowserAgentPool;
 import com.gauge.crawler.commons.Content;
-import com.gauge.crawler.exception.GaugeCrawlerException;
+import com.gauge.crawler.exception.GaugeParsingException;
 import com.gauge.crawler.gaugefile.FilePathHandeler;
 import com.jaunt.Elements;
 import com.jaunt.ResponseException;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 /**
  *
@@ -84,8 +89,41 @@ public class DataConentFile implements Content {
         }
     }
 
-    // This method will convert text fro pdf
-    public void getTextFromPdf() {
+    // This method will extract text from pdf at runtime after just downloading pdf file
+    public void getTextFromPdf(String pdfPath) throws GaugeParsingException {
+        this.textData = "";
+        PDFParser parser;
+        PDFTextStripper pdfStripper;
+        PDDocument pdDoc = null;
+        COSDocument cosDoc = null;
+        try {
+            parser = new PDFParser(new FileInputStream(pdfPath));
+        } catch (IOException e) {
+            System.err.println("Unable to open PDF Parser. " + e.getMessage());
+            throw new GaugeParsingException();
+        }
+        try {
+            parser.parse();
+            cosDoc = parser.getDocument();
+            pdfStripper = new PDFTextStripper();
+            pdDoc = new PDDocument(cosDoc);
+            pdfStripper.setStartPage(1);
+            pdfStripper.setEndPage(5);
+            this.textData = pdfStripper.getText(pdDoc);
+        } catch (Exception e) {
+            System.out.println("Something wrong in this pdf document");
+            this.textData = "";
+        } finally {
+            try {
+                if (cosDoc != null) {
+                    cosDoc.close();
+                }
+                if (pdDoc != null) {
+                    pdDoc.close();
+                }
+            } catch (Exception e) {
+            }
+        }
 
     }
 
